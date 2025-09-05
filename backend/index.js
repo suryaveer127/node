2d938b0a30e379a8a5a35e87b2fea984e1473cb8
suryaveer127 <suryaveersinghrathore377@gmail.com>
2025-09-05 15:48:31 +0530

    Initial commit of MERN project

--- backend/index.js
+++ backend/index.js
@@ -140,7 +140,7 @@ io.on('connection', (socket) => {
 });
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 app.get(/.*/, (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontned/build", "index.html"));
+  res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
 });
 
 
