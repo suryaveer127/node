1b88e5ee885eb55eb22b53d619a05db8beb6b780
suryaveer127 <suryaveersinghrathore377@gmail.com>
2025-09-08 17:24:31 +0530

    production

--- backend/index.js
+++ backend/index.js
@@ -76,6 +76,7 @@ const mongoose = require('mongoose');
 const http = require('http');
 const cors = require('cors');
 const { Server } = require('socket.io');
+const __dirname=path.resolve();
 
 const authRoutes = require('./routes/authRoutes');
 const otpRoutes = require('./routes/otpRoutes');
@@ -140,10 +141,10 @@ io.on('connection', (socket) => {
     io.to("admin_room").emit("updateLiveUsers", Array.from(liveUsers.values()));
   });
 });
-// app.use(express.static(path.join(__dirname, "../frontend/dist")));
-// app.get(/.*/, (req, res) => {
-//   res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
-// });
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend","dist", "index.html"));
+});
 
 
 
