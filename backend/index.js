f1ca43e317c1b755f94cd3fad5b04f989fb8ae8e
suryaveer127 <suryaveersinghrathore377@gmail.com>
2025-09-08 14:15:27 +0530

    Your commit message

--- backend/index.js
+++ backend/index.js
@@ -73,9 +73,10 @@ io.on('connection', (socket) => {
   });
 });
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
 });
+
 
 
 
