0a3f5dbe0265524c4692184e25997c075d8b4ffe
suryaveer127 <suryaveersinghrathore377@gmail.com>
2025-09-09 19:28:33 +0530

    Initial commit

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { redisUser, registration, login, getAllUsers } = require('../controllers/authController');
+const { redisUser, registration, login, getAllUsers ,logout} = require('../controllers/authController');
 
 router.post('/register-temp', redisUser);
 router.post('/register-final', registration);
 router.post('/login', login);
+router.post('/logout',logout);
 router.get('/users', getAllUsers);
 
 module.exports = router;
